ec3cb75861e1397f4802c72a99f4f669e023be3f
Debamitro Chakraborti <debamitro@gmail.com>
2025-07-29 10:10:48 -0400

    updated version to 0.2-beta

--- src/main.rs
+++ src/main.rs
@@ -73,6 +73,6 @@ fn about(_: &About, cx: &mut App) {
     let _ = MessageDialog::new()
         .set_type(MessageType::Info)
         .set_title("About Gmail Cleaner")
-        .set_text(&format!("Gmail Cleaner v0.1.0-beta\n\n{}", about_text))
+        .set_text(&format!("Gmail Cleaner v0.2.0-beta\n\n{}", about_text))
         .show_alert();
 }
